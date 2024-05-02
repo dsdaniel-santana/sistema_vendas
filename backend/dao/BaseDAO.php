6cd0d54f2740e283549c85ad0c806ec33c3d8bf6
Daniel Santana <daniel_santana_87@hotmail.com>
2024-05-02 20:43:39 -0300

    BASE DATA ACCESS OBJECT

---
+++ backend/dao/BaseDAO.php
@@ -0,0 +1,10 @@
+<?php
+    interface BaseDAO{
+        public function getById($id);        
+        public function getAll();        
+        public function create($entity);        
+        public function update($entity);        
+        public function delete($id);        
+    }
+
+?>
